d8f00a87c57a16fed3acd67e4c71759e2b22f0e3
Gard Sæbbe <g.saebbe@hotmail.com>
2025-10-06 16:35:29 +0200

    #6

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gardsaebbe.github.io/',
-  base: '/website/',
+  base: '/website',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
